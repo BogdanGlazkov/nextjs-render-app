6259987308e4db14f3ce53c20fb56d732fa2d594
BogdanGlazkov <bglazkov@i.ua>
2023-05-02 22:24:01 +0300

    Add static rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Next.js pre-rendering</h1>;
+  return (
+    <>
+      <h1>Next.js pre-rendering</h1>
+      <Link href="/users">
+        <p>Users</p>
+      </Link>
+    </>
+  );
 }
 
 export default Home;
