e0ebbbb6eb59a0c368d4a0d940280cf5697e1a18
BogdanGlazkov <bglazkov@i.ua>
2023-05-03 23:00:02 +0300

    Add News page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -20,7 +20,6 @@ function ProductList({ products }) {
 export default ProductList;
 
 export async function getStaticProps() {
-  console.log("Generating / Regenerating ProductList");
   const response = await fetch("http://localhost:4000/products");
   const data = await response.json();
 
